56a7d6880e9421079072a023f3e1c8ca0bcd8581
Carlos Henrique <carloshenriquechhp@gmail.com>
2020-03-10 01:43:35 -0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	const body = document.querySelector('body');
-	if (window.innerWidth < 990) {
-		const body = document.querySelector('body');
-		body.style.opacity = '0';
-		body.style.overflow = 'hidden';
-	} else {
-		body.style.opacity = '1';
-		body.style.overflow = 'visible';
-	}
-	window.addEventListener('resize', function() {
-		if (window.innerWidth < 990) {
-			const body = document.querySelector('body');
-			body.style.opacity = '0';
-			body.style.overflow = 'hidden';
-		} else {
-			body.style.opacity = '1';
-			body.style.overflow = 'visible';
-		}
-	});
-});
+// document.addEventListener("DOMContentLoaded", function(event) {
+// 	const body = document.querySelector('body');
+// 	if (window.innerWidth < 990) {
+// 		const body = document.querySelector('body');
+// 		body.style.opacity = '0';
+// 		body.style.overflow = 'hidden';
+// 	} else {
+// 		body.style.opacity = '1';
+// 		body.style.overflow = 'visible';
+// 	}
+// 	window.addEventListener('resize', function() {
+// 		if (window.innerWidth < 990) {
+// 			const body = document.querySelector('body');
+// 			body.style.opacity = '0';
+// 			body.style.overflow = 'hidden';
+// 		} else {
+// 			body.style.opacity = '1';
+// 			body.style.overflow = 'visible';
+// 		}
+// 	});
+// });
